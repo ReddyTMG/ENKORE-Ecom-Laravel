24359ea122540f74c4d8c5dfa468e949b55fb683
Maple <82824509+ReddyTMG@users.noreply.github.com>
2025-08-03 17:50:23 +0800

    bootstrap, config, DB, public, resources, route, storage, test, Not include vendor and npm

---
+++ storage/framework/views/afd382d5d99ae9b3782d5cd7d8922b7e.php
@@ -0,0 +1,25 @@
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card border border-info-subtle">
+                <div class="card-header"><?php echo e(__('Dashboard')); ?></div>
+
+                <div class="card border border-info-subtle">
+                    <?php if(session('status')): ?>
+                        <div class="alert alert-success" role="alert">
+                            <?php echo e(session('status')); ?>
+
+                        </div>
+                    <?php endif; ?>
+
+                    <?php echo e(__('You are logged in!')); ?>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH D:\Utar\Degree\Year_3\s2\AW\a\ecom\ecom\resources\views/home.blade.php ENDPATH**/ ?>
